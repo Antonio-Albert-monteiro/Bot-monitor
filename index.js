49fb15505c2f0962927c7bd692b2fff17b667fb5
Antonio Albert <antonioalbert349@gmail.com>
2021-04-01 20:47:11 +0000

    Get comcluido

--- index.js
+++ index.js
@@ -2,15 +2,23 @@ const Discord = require("discord.js"); //Conexão com a livraria Discord.js
 const client = new Discord.Client(); //Criação de um novo Client
 const axios = require("axios");
 
-client.on('ready', () => {
+var fun = null;
+
+function tesu() {
+  fun = setInterval(apiall, 9000);
+}
+
+function apiall() {
     const data = axios({
         url: "https://testapi.antonio9594.repl.co/",
         headers: {'accept': 'application/json'}
     }).then((result) => {
-        console.log(result)
+        console.log(result.data)
     }).catch((error) => {
         console.error(error)
     })
-})
+}
+
+tesu();
 
 client.login(process.env.TOKEN); //Ligando o Bot caso ele consiga acessar o token
